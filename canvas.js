1d2cc5ce8fdc109f87d12bb317b288dc74674858
Kevin Lee <kllee@ucdavis.edu>
2017-12-22 13:19:32 -0800

    Get and display list of courses

---
+++ canvas.js
@@ -0,0 +1,28 @@
+function getCoursesList() {
+    // function that is called when data is returned
+    function requestListener() {
+        // remove while(1); from beginning of returned JSON string
+        var JSONresponseArray = JSON.parse(this.responseText.split('while(1);',2)[1]);
+
+        // get div for course list
+        var pgh = document.getElementById('courses');
+
+        // add course names to page
+        for (i = 0; i < JSONresponseArray.length; i++) {
+            if (JSONresponseArray[i].hasOwnProperty('name')) {
+                var node = document.createElement('p');
+                var nodeText = document.createTextNode(JSONresponseArray[i].name);
+                node.appendChild(nodeText);
+                pgh.appendChild(node);
+            }
+        }
+    }
+
+	var request = new XMLHttpRequest();
+    // call requestListener when request is loaded
+	request.addEventListener('load', requestListener);
+	request.open('GET', 'https://canvas.ucdavis.edu/api/v1/courses/?page=1&per_page=100');
+    request.send();
+}
+
+getCoursesList();
