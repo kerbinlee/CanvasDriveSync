ada30c01247308cca4ef04f97743d458a6cd4886
Kevin Lee <kllee@ucdavis.edu>
2017-12-22 16:27:02 -0800

    Load list of files for each course

--- canvas.js
+++ canvas.js
@@ -1,19 +1,49 @@
+function getFilesList(courseDiv, courseId) {
+    // function that is called when data is returned
+    function requestListener() {
+        // remove while(1); from beginning of returned JSON string
+        var JSONresponseArray = JSON.parse(this.responseText.split('while(1);',2)[1]);
+
+        // add file names to course div
+        for (i = 0; i < JSONresponseArray.length; i++) {
+            var node = document.createElement('p');
+            console.log(JSONresponseArray[i].display_name);
+            var nodeText = document.createTextNode(JSONresponseArray[i].display_name);
+            node.appendChild(nodeText);
+            courseDiv.appendChild(node);
+        }
+    }
+
+	var request = new XMLHttpRequest();
+    // call requestListener when request is loaded
+	request.addEventListener('load', requestListener);
+	request.open('GET', 'https://canvas.ucdavis.edu/api/v1/courses/' + courseId + '/files?page=1&per_page=100');
+    request.send();
+}
+
 function getCoursesList() {
     // function that is called when data is returned
     function requestListener() {
         // remove while(1); from beginning of returned JSON string
         var JSONresponseArray = JSON.parse(this.responseText.split('while(1);',2)[1]);
+        // sort courses by term
         JSONresponseArray.sort(function(a, b){return b.enrollment_term_id - a.enrollment_term_id});
         // get div for course list
-        var pgh = document.getElementById('courses');
+        var coursesDiv = document.getElementById('courses');
 
         // add course names to page
         for (i = 0; i < JSONresponseArray.length; i++) {
             if (JSONresponseArray[i].hasOwnProperty('name')) {
-                var node = document.createElement('p');
-                var nodeText = document.createTextNode(JSONresponseArray[i].name);
-                node.appendChild(nodeText);
-                pgh.appendChild(node);
+                var courseDiv = document.createElement('div');
+                var courseNameHeader = document.createElement('h1');
+                var courseName = document.createTextNode(JSONresponseArray[i].name);
+                courseNameHeader.appendChild(courseName);
+                courseDiv.appendChild(courseNameHeader);
+                coursesDiv.appendChild(courseDiv);
+
+                var courseId = JSONresponseArray[i].id;
+                // get list of files for course
+                getFilesList(courseDiv, courseId);
             }
         }
     }
